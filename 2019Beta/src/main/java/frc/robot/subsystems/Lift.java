db55a5c491e3a0a27b0a77aa70a6e32833b20138
Phillip White <boring.someone@gmail.com>
2018-09-15 16:11:54 -0400

    Commit to teh Lift locks

--- 2019Beta/src/main/java/frc/robot/subsystems/Lift.java
+++ 2019Beta/src/main/java/frc/robot/subsystems/Lift.java
@@ -7,6 +7,7 @@ import frc.lib.loops.ILooper;
 import frc.lib.loops.Loop;
 import frc.lib.util.HIDHelper;
 import frc.robot.Constants;
+import frc.robot.RobotMap;
 
 public class Lift extends Subsystem {
     //The one instance of Lift
@@ -54,13 +55,17 @@ public class Lift extends Subsystem {
         setOperatorInput(HIDHelper.getAdjStick(Constants.SECOND_STICK));
         periodic.trigger = Constants.SECOND.getTrigger();
         periodic.liftEncoder = lowerLift.get();
+        periodic.buttonArray[0] = liftUpper.getSensorCollection().isFwdLimitSwitchClosed();
+        periodic.buttonArray[1] = liftUpper.getSensorCollection().isRevLimitSwitchClosed();
+        periodic.buttonArray[2] = liftLower.getSensorCollection().isFwdLimitSwitchClosed();
+        periodic.buttonArray[3] = liftLower.getSensorCollection().isRevLimitSwitchClosed();
     }
 
     // Optional design pattern for caching periodic writes to avoid hammering the HAL/CAN.
     public void writePeriodicOutputs() {
 
-        if (periodic.trigger) {
-            liftUpper.set(-1.5*operatorInput[1]);
+        if (periodic.trigger || LiftDisable()) {
+            liftUpper.set(-1.5 * operatorInput[1]);
             liftLower.set(operatorInput[1]);
         } else {
             liftLower.set(operatorInput[1]);
@@ -68,10 +73,19 @@ public class Lift extends Subsystem {
         }
     }
 
+    private boolean LiftDisable() {
+        return ((periodic.buttonArray[2] && !periodic.buttonArray[1]) || (periodic.buttonArray[3] && !periodic.buttonArray[0])) &&
+                !SmartDashboard.getBoolean("DB/Button 0", false);
+    }
+
 
     public void outputTelemetry() {
-        SmartDashboard.putNumber("LiftEncoder" , periodic.liftEncoder);
-        SmartDashboard.putNumberArray("LiftUpper" , operatorInput);
+        SmartDashboard.putNumber("LiftEncoder", periodic.liftEncoder);
+        SmartDashboard.putNumberArray("LiftUpper", operatorInput);
+        SmartDashboard.putBoolean("Stage 1 Low", periodic.buttonArray[3]);
+        SmartDashboard.putBoolean("Stage 1 High", periodic.buttonArray[2]);
+        SmartDashboard.putBoolean("Stage 2 Low", periodic.buttonArray[0]);
+        SmartDashboard.putBoolean("Stage 2 High", periodic.buttonArray[1]);
     }
 
     public void stop() {
@@ -91,6 +105,8 @@ public class Lift extends Subsystem {
         //INPUTS
         public boolean trigger = false;
         public int liftEncoder = 0;
+        public boolean[] buttonArray = new boolean[4];
+
         //OUTPUTS
 
     }
