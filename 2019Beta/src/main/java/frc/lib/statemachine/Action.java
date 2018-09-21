c0f782623c60cb6182b0bcc456b2a1a981942742
coalman321 <coalman321@users.noreply.github.com>
2018-09-21 12:39:09 -0400

    added toCommand for button compatability

--- 2019Beta/src/main/java/frc/lib/statemachine/Action.java
+++ 2019Beta/src/main/java/frc/lib/statemachine/Action.java
@@ -1,8 +1,34 @@
 package frc.lib.statemachine;
 
-public interface Action {
-    void onStart();
-    void onLoop();
-    boolean isFinsihed();
-    void onStop();
+import edu.wpi.first.wpilibj.command.Command;
+
+public abstract class Action {
+    abstract void onStart();
+    abstract void onLoop();
+    abstract boolean isFinsihed();
+    abstract void onStop();
+
+    /**
+     * converts an action to a wpilib command for buttons
+     */
+    public static Command toCommand(Action action){
+        return new Command() {
+
+            protected void initialize(){
+                action.onStart();
+            }
+
+            protected void execute(){
+                action.onLoop();
+            }
+
+            protected boolean isFinished() {
+                return action.isFinsihed();
+            }
+
+            protected void end(){
+                action.onStop();
+            }
+        };
+    }
 }
