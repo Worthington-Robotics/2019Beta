304d50ed2e495d3774672b4d2811ecd222257694
Phillip White <boring.someone@gmail.com>
2018-10-02 10:38:14 -0400

    Work on auto

--- 2019Beta/src/main/java/frc/robot/planners/TraGenerator.java
+++ 2019Beta/src/main/java/frc/robot/planners/TraGenerator.java
@@ -38,6 +38,6 @@ public class TraGenerator {
         List<Pose2d> Points = new ArrayList<>();
         Points.add(new Pose2d(0, 0, Rotation2d.fromDegrees(0)));
         Points.add(new Pose2d(120 , 0, Rotation2d.fromDegrees(0)));
-        return generateTrajectory(false, Points, Arrays.asList(new CentripetalAccelerationConstraint(100.0)), 40.0, 40.0, 9.0);
+        return generateTrajectory(false, Points, Arrays.asList(new CentripetalAccelerationConstraint(1000.0)), 40.0, 40.0, 9.0);
     }
 }
